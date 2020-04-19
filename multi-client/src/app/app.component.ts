53c2bdf6cc29aa9605e3206a9dd949558133ce55
dsykex <dezmensykes.ds@gmail.com>
2020-04-19 15:21:34 -0400

    Changes..

--- multi-client/src/app/app.component.ts
+++ multi-client/src/app/app.component.ts
@@ -25,12 +25,10 @@ export class AppComponent {
   adj = 'people';
 
   img: SafeResourceUrl;
-  recognizer: any;
-  examples=[];
+ 
   constructor()
   {
-    this.initRezognizer();
-
+   
     this.socket = io('https://mighty-thicket-03422.herokuapp.com/');
 
     this.socket.on('width_value', val => {
@@ -49,37 +47,6 @@ export class AppComponent {
     this.socket.on('update_picture', imgData => {
       this.img = imgData;
     })
-  }
-
-  collect(label) {
-    if (this.recognizer.isListening()) {
-      return this.recognizer.stopListening();
-    }
-    if (label == null) {
-      return;
-    }
-    this.recognizer.listen(async ({spectrogram: {frameSize, data}}) => {
-      let vals = this.normalize(data.subarray(-frameSize * 3));
-      //console.log(this.examples);
-      this.examples.push({vals, label});
-      this.console.nativeElement.textContent =
-          `${this.examples.length} examples collected`;
-    }, {
-      overlapFactor: 0.999,
-      includeSpectrogram: true,
-      invokeCallbackOnNoiseAndUnknown: true
-    });
-   }
-
-   normalize(x) {
-    const mean = -100;
-    const std = 10;
-    return x.map(x => (x - mean) / std);
-   }
-  async initRezognizer(){
-    this.recognizer = speechCommands.create('BROWSER_FFT');
-    await this.recognizer.ensureModelLoaded();
-    //this.predictWord();
   }
 
   updateWidth(val)
